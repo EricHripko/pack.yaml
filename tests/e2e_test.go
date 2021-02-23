f91ef2ad728635762372ebb990d236a8d899099d
Eric Hripko <eric@hripko.com>
2021-02-23 09:38:49 +0000

    Finalise the test

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -2,53 +2,56 @@ package e2e
 
 import (
 	"bytes"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestEnd2End(t *testing.T) {
+func build(t *testing.T, project string) {
 	// Arrange
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		t.Fatal("cannot discover tests")
-	}
+	var stderr bytes.Buffer
+	var stdout bytes.Buffer
+	cmd := exec.Command(
+		"docker",
+		"build",
+		"-t",
+		project,
+		"-f", "pack.yaml",
+		".",
+	)
+	cmd.Dir = filepath.Join(".", project)
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+	cmd.Env = append(os.Environ(), "DOCKER_BUILDKIT=1")
 
 	// Act
-	for _, file := range files {
-		if !file.IsDir() {
-			continue
-		}
+	err := cmd.Run()
 
-		t.Run(file.Name(), func(t *testing.T) {
-			// Arrange
-			var stderr bytes.Buffer
-			var stdout bytes.Buffer
-			cmd := exec.Command(
-				"docker",
-				"build",
-				"-t",
-				file.Name(),
-				"-f", "pack.yaml",
-				".",
-			)
-			cmd.Dir = filepath.Join(".", file.Name())
-			cmd.Stderr = &stderr
-			cmd.Stdout = &stdout
-			cmd.Env = append(os.Environ(), "DOCKER_BUILDKIT=1")
-
-			// Act
-			err = cmd.Run()
-
-			// Assert
-			if err != nil {
-				t.Log("stdout: ", stdout.String())
-				t.Log("stderr: ", stderr.String())
-				t.Log(err)
-				t.Fail()
-			}
-		})
+	// Assert
+	if err != nil {
+		t.Log("stdout: ", stdout.String())
+		t.Log("stderr: ", stderr.String())
+		t.Log(err)
+		t.Fail()
 	}
+}
+
+func TestGoCli(t *testing.T) {
+	// Arrange
+	project := "go-cli-template-master"
+	build(t, project)
+
+	var stdout bytes.Buffer
+	cmd := exec.Command("docker", "run", "--rm", project)
+	cmd.Stdout = &stdout
+
+	// Act
+	err := cmd.Run()
+
+	// Assert
+	require.Nil(t, err)
+	require.Contains(t, stdout.String(), "cli-template")
 }
